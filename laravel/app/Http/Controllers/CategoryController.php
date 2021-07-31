54fa425d8461249b28272f9edca376dcc707d137
Masaya Daiguji <masa134578@gmail.com>
2021-07-31 18:49:18 +0900

    CategoryControllerの各メソッドのルーティング

--- laravel/app/Http/Controllers/CategoryController.php
+++ laravel/app/Http/Controllers/CategoryController.php
@@ -13,7 +13,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        return view('categories.form');
     }
 
     /**
@@ -23,7 +23,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('categories.form');
     }
 
     /**
@@ -56,7 +56,7 @@ class CategoryController extends Controller
      */
     public function edit($id)
     {
-        //
+        return view('categories.form');
     }
 
     /**
