c34bdc99c0b7f4df53297b29c06acb1a1ea84f3c
Masaya Daiguji <masa134578@gmail.com>
2021-08-02 16:53:15 +0900

    CategoriesにCreateとEditのファイルを追加

---
+++ laravel/resources/views/categories/edit.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- ジャンル記入欄 -->
+<div class="card-body text-dark">
+    <table class="table">
+        <tr>
+            <th>好きなジャンル</th>
+            <td><input type="text" placeholder="好きなジャンル"></td>
+        </tr>
+
+    </table>
+</div>
+
+@endsection()
